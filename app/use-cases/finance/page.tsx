50c8e066897634781554c1d7dc625c404f8c0e7d
nazif sohtaoglu <sohtanazif@gmail.com>
2026-05-12 18:40:03 +0100

    Use permanent redirect for finance use case

--- app/use-cases/finance/page.tsx
+++ app/use-cases/finance/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from 'next/navigation'
+import { permanentRedirect } from 'next/navigation'
 
 export default function FinanceUseCaseRedirect() {
-  redirect('/use-cases/financial-services')
+  permanentRedirect('/use-cases/financial-services')
 }
